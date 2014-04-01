11909cdb0f052753ec895080df8d83e6ebad5749
Jaclyn Rosa <Emmitt.lemon.fox@gmail.com>
2014-03-31 19:27:13 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FTLstore::Application.config.secret_key_base = '2db8864db700f1e9b35aebc07aa7966fbdd82f090594a2bee47f2f97962104ff1f71fea5a4b1a55f89058ea0f27f9e30a29d4585a84ba9cd961ba8b47c552873'
