2e3aefc879c40dc6ba8df9bdefc0f55960e06c3e
Jaclyn Rosa <Emmitt.lemon.fox@gmail.com>
2014-05-12 16:58:51 -0500

    Finished Project

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,12 +8,12 @@ class SessionsController < ApplicationController
 		redirect_to new_user_path
 	else
 		user = User.find_by(name: params[:name])
-	end
-	if user and user.authenticate(params[:password])
-		session[:user_id] = user.id
-		redirect_to admin_url
-	else
-		redirect_to login_url, alert: "Invalid user/password combination"
+		if user and user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect_to admin_url
+		else
+			redirect_to login_url, alert: "Invalid user/password combination"
+		end
 	end
   end
 
